7b5fcda791b165a0873a3c336084563f7b9cd810
CaioJN <42421508+CaioJN@users.noreply.github.com>
2018-11-14 08:31:28 -0200

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,23 @@
+/*.P3.c*/
+/*Caio José Nogueira Machado*/
+/*11811EEL003*/
+#include <stdio.h>
+
+int main()
+{
+	int recebe = 0, i;
+	char txt[256];
+	
+	scanf("%s", &txt);
+	
+	for(i=0; txt[i] != '\0'; i++)
+	{
+		if(txt[i]>='0' && txt[i]<='9')
+		{
+			recebe = (recebe*10) + txt[i] - 48;
+		}
+	}
+	printf("%i", recebe);
+	
+	return 0;
+}
