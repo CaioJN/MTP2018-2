4677c82c046e1292d2fc9731eb0ce13082f40e7d
CaioJN <42421508+CaioJN@users.noreply.github.com>
2018-12-18 12:19:59 -0200

    Add files via upload

---
+++ p6.c
@@ -0,0 +1,40 @@
+/*.p5.c*/
+/*Caio José Nogueira Machado*/
+/*11811EEL003*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct ponto
+{
+	double x;
+	double y;
+};
+
+void gera_pontos(struct ponto *v, int n)
+{
+	int i;
+	for(i=0;i<n;i++)
+	{
+		v[i].x=cos(i*2.0*M_PI/(n-1));
+		v[i].y=sin(i*2.0*M_PI/(n-1));
+	}
+	
+}
+
+int main()
+{
+	int n, i;
+	scanf("%d", &n);
+	struct ponto *v = (struct ponto *)calloc(n, sizeof(struct ponto));
+	gera_pontos(v,n);
+	
+	printf("\n");
+	for(i=0;i<n;i++)
+	{
+		printf("(%.3lf,%.3lf) ", v[i].x, v[i].y);
+	}
+	free(v);
+	return 0;
+}
